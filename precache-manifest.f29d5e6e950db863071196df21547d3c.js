716eb9720ae33a1db3a8bf493c1c3e5aadc4058d
Tej <tejmpatel98@gmail.com>
2020-04-22 21:48:05 +0530

    Updates

---
+++ precache-manifest.f29d5e6e950db863071196df21547d3c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2988a1fbed876d7b476fb1305167a7",
+    "url": "/Tejp98/Keeper.git/index.html"
+  },
+  {
+    "revision": "c6bfd9e5eb9b68762b0c",
+    "url": "/Tejp98/Keeper.git/static/js/2.0a2e219e.chunk.js"
+  },
+  {
+    "revision": "3d405616a9a17b38aeea",
+    "url": "/Tejp98/Keeper.git/static/js/main.40279a3c.chunk.js"
+  },
+  {
+    "revision": "45e7be9e0c4cee3205a0",
+    "url": "/Tejp98/Keeper.git/static/js/runtime-main.00c1a8db.js"
+  }
+]);
