45ca531fe4e8d675496772e46fa74e98ac70bfec
Tej <tejmpatel98@gmail.com>
2020-04-22 22:15:23 +0530

    Updates

---
+++ precache-manifest.b8cdb4433a06d704195a4662e7dc15db.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301193283d3ab6762d3a8abbf0c7103f",
+    "url": "/Tejp98/Keeper/public.git/index.html"
+  },
+  {
+    "revision": "c6bfd9e5eb9b68762b0c",
+    "url": "/Tejp98/Keeper/public.git/static/js/2.0a2e219e.chunk.js"
+  },
+  {
+    "revision": "3d405616a9a17b38aeea",
+    "url": "/Tejp98/Keeper/public.git/static/js/main.40279a3c.chunk.js"
+  },
+  {
+    "revision": "8b93cb92c8f3505e0314",
+    "url": "/Tejp98/Keeper/public.git/static/js/runtime-main.52348673.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Tejp98/Keeper/tree/master/public.git/precache-manifest.5605b3319c717db1020391ff9acd438c.js"
+  "/Tejp98/Keeper/public.git/precache-manifest.b8cdb4433a06d704195a4662e7dc15db.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tejp98/Keeper/tree/master/public.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Tejp98/Keeper/public.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
